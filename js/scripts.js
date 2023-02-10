1e62a8992f05792ec5a37c402c11cefbc07092ac
Zackary Ross <zross03@icloud.com>
2023-02-09 20:53:01 -0500

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -17,18 +17,23 @@ for (i = 0; i < coll.length; i++) {
 
 
 
+// Get the modal
+var modal = document.getElementById("myModal");
 
-$(function () {
-    "use strict";
-    
-    $(".popup img").click(function () {
-        var $src = $(this).attr("src");
-        $(".show").fadeIn();
-        $(".img-show img").attr("src", $src);
-    });
-    
-    $("span, .overlay").click(function () {
-        $(".show").fadeOut();
-    });
-    
-});
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
