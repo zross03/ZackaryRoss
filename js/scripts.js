2f17f1d7b57e531383a25fcebaff8490d470d0a8
Zackary Ross <zross03@icloud.com>
2023-02-17 09:26:25 -0500

    changes to contact form

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,56 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+
+let contactFields = {};
+
+document.addEventListener ("DOMContentLoaded", function () {
+  fields.name = document. getElementById('name');
+  fields.email = document.getElementById('email');
+  fields.subject = document.getElementById('subject');
+  fields.message = document.getElementById('message');
+  })
+
+  function isNotEmpty(value) {
+    if (value == null || typeof value == 'undefined' ) return false;
+    return (value.length > 0);
+   }
+
+   function isEmail(email) {
+    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    return regex.test(String(email).toLowerCase());
+   }
+
+   function fieldValidation(field, validationFunction) {
+    if (field == null) return false;
+   
+    let isFieldValid = validationFunction(field.value)
+    if (!isFieldValid) {
+    field.className = 'placeholderRed';
+    } else {
+    field.className = '';
+    }
+   
+    return isFieldValid;
+   }
+
+   function isValid() {
+    var valid = true;
+    
+    valid &= fieldValidation(fields.name, isNotEmpty);
+    valid &= fieldValidation(fields.email, isEmail);
+    valid &= fieldValidation(fields.subject, isNotEmpty);
+    valid &= fieldValidation(fields.message, isNotEmpty);
+    return valid;
+   }
+
+   function sendContact () {
+    if (isValid()) {
+    let us = new User(name.value, email.value, subject.value,
+    message.value);
+    alert('${usr.name] thanks for your message.")
+    } else {
+    alert("There was an error")
+    }
+  }
