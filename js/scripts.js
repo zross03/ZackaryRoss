ccdfee88aa9c2d4fd1d5435053cca1d7ea717d2f
Zackary Ross <122134752+zross03@users.noreply.github.com>
2023-03-09 00:52:05 -0500

    gallery pages and modal image

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ let i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    let content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
@@ -12,3 +12,34 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+
+
+
+
+
+/* thumbnail gallery code */
+const images = document.querySelectorAll(".gallery-item img");
+let imgSrc;
+// get images src onclick
+images.forEach((img) => {
+    img.addEventListener("click", (e) => {
+        imgSrc = e.target.src;
+        document.getElementById("modal").id = 'modalActive';
+        document.getElementById("modalControl").id = 'modalControlActive';
+
+
+
+      const fileName = imgSrc.split("/").pop();
+      let fileLoc = imgSrc.split("images/").pop();
+      let imgFolder = fileLoc.split("/").shift();
+      let modalSrc = "../../images/" + imgFolder + "/large/" + fileName;
+        document.getElementById("modalImage").src = modalSrc;
+
+     });
+});
+
+function closeModal()  {
+  document.getElementById("modalControlActive").id = 'modalControl';
+  document.getElementById("modalActive").id = 'modal';
+  }
