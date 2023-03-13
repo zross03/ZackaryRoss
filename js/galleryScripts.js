649bee79c325b4fd13a8dcaf5b09a387cfbfbce1
Zackary Ross <122134752+zross03@users.noreply.github.com>
2023-03-12 21:22:39 -0400

    fixed javascript issue and seperated script file

--- js/galleryScripts.js
+++ js/galleryScripts.js
@@ -1,24 +1,3 @@
-// collapsible resume sections
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
-
-
-
-
-
-
 // thumbnail gallery code
 const images = document.querySelectorAll(".gallery-item img");
 let imgSrc;
